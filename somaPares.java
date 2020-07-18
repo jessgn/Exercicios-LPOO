df1726790656d04c507eae70c418779225ba8578
Jéssica Nascimento <59929856+jessgn@users.noreply.github.com>
2020-07-18 20:04:31 -0300

    Update and rename Soma_Pares.java to somaPares.java

--- somaPares.java
+++ somaPares.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.lang.String;
 import java.util.*;
 
-public class E3{
+public class somaPares{
   public static void main (String args[]){
     int i;
     Scanner s = new Scanner (System.in);
